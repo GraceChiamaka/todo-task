d6f177cb4f3c0b3979f20554617fb04859f05195
GraceChiamaka <obieri.grace@gmail.com>
2022-03-16 09:43:37 +0100

    update test for view task component

--- test/ViewTask.test.tsx
+++ test/ViewTask.test.tsx
@@ -1,32 +1,26 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { ViewTask } from "../src/components";
 import { Provider } from "react-redux";
-import { useStore } from "react-redux";
+import { makeStore } from "../src/store";
+
+const store = makeStore({});
+
+const ReduxProvider = ({ children, reduxStore }) => (
+  <Provider store={reduxStore}>{children}</Provider>
+);
 
 const MockCard = () => {
-  // const store = useStore();
-  return (
-    // <Provider store={store}>
-    <ViewTask title="Clear our input" completed={true} id="2" />
-    // </Provider>
-  );
+  return <ViewTask title="Clear our input" completed={true} id="2" />;
 };
 
-// it("should render taskdetails passed into task props", () => {
-//   render(<MockCard />);
-//   const heading = screen.getByText(/Clear our input/i);
-//   expect(heading).toBeInTheDocument();
-// });
-
-it("should render page header component with text passed into subTitle prop", () => {
-  //   render(
-  //     <PageHeader
-  //       title="Page Header"
-  //       subTitle="Welcome"
-  //       showPageModal={() => console.log("hello")}
-  //     />
-  //   );
-  //   const paragraph = screen.getByText(/Welcome/i);
-  //   expect(paragraph).toBeInTheDocument();
+it("should render taskdetails passed into task props", () => {
+  render(
+    <ReduxProvider reduxStore={store}>
+      <MockCard />
+    </ReduxProvider>
+  );
+  const heading = screen.getByText(/Clear our input/i);
+  expect(heading).toBeInTheDocument();
 });
